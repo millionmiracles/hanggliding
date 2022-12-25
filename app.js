c24303afc266a4509c7647fbcc39a0d8d25a0851
Kiril <kaloyanov.kiril@gmail.com>
2022-12-25 19:19:58 +0200

    added favicon

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "..", "static");
+const publicPath = path.join(__dirname, "static");
 
 app.use(express.static(publicPath));
 

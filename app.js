2664365ffdd39ea56d77e094622a9f251f39e231
Kiril <kaloyanov.kiril@gmail.com>
2022-12-25 19:07:06 +0200

    fix the server

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const path = require("path");
 const express = require("express");
 const app = express();
+const publicPath = path.join(__dirname, "..", "static");
 
-app.use(express.static("public"));
+app.use(express.static(publicPath));
 
-app.get("/", (req, res) => res.send("<h1>Hi</h1>"));
+app.get("*", (req, res) => res.send(path.join(publicPath, "index.html")));
 
-app.listen(process.env.PORT || 3000, ()=> console.log('Server is running...'))
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
